868ded84e9968a901486dedd941958a505e8176f
Rémi Vion <vion.remi@gmail.com>
2015-10-29 17:07:37 +0100

    clean getIpOnInterface script

--- docker-images/spark-full/scripts/getIpOnInterface.sh
+++ docker-images/spark-full/scripts/getIpOnInterface.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e # fail when any subcommand fails
+set -u # fail when an unknown variable is referenced
+
 ifconfig $1 | awk '/inet / {print $2}' | awk '{gsub("addr:","");print}'
