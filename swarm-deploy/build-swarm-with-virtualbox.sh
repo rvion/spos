783854647424027ab3d27a6e75c33aa60848e9c1
Rémi Vion <vion.remi@gmail.com>
2015-10-29 17:53:30 +0100

    fix build-swarm-with-virtualbox script

--- swarm-deploy/build-swarm-with-virtualbox.sh
+++ swarm-deploy/build-swarm-with-virtualbox.sh
@@ -2,12 +2,13 @@
 
 set -e # fail when any subcommand fails
 set -u # fail when an unknown variable is referenced
+set -x
 
-# This is just an example script that bootstrap 5 machines on virtualbox
+# This is just an example script that bootstrap 3 machines on virtualbox
 
 SWARM_TOKEN=$(docker run --rm swarm create)
 
-dm create \
+docker-machine create \
   --driver=virtualbox \
   --virtualbox-disk-size "12000" \
   --virtualbox-memory "1024" \
@@ -15,8 +16,8 @@ dm create \
   --swarm-master \
   mm
 
-for i in {1..4}; do
-  dm create \
+for i in {1..2}; do
+  docker-machine create \
     --driver=virtualbox \
     --virtualbox-disk-size "12000" \
     --virtualbox-memory "1024" \
